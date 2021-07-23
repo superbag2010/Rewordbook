e5e520e7cc3d5830701ef1129e4e1369e840e5fe
choi <superbag2010@naver.com>
2021-07-24 03:05:07 +0900

    append mode

--- rewordbook/manager/constants.py
+++ rewordbook/manager/constants.py
@@ -14,7 +14,7 @@ OUTPUT_URL=r"D:\Googleドライブ\日本語\rewordbook.csv"
 OUTPUT_BACKUP_URL=r"D:\Googleドライブ\日本語\rewordbook" + str(date.today()) + ".csv"
 
 # output format
-FIELDS=['word', 'memorized', 'url', 'date', 'count']
+FIELDS=['memorized', 'word', 'url', 'date', 'count']
 
 # sql
 GET_HISTORY_SQL_CHROME="SELECT DISTINCT urls.title, urls.url, urls.last_visit_time, urls.visit_count FROM urls, visits WHERE urls.id = visits.url and urls.url LIKE ('%ja.dict.naver.com%');"
@@ -24,3 +24,5 @@ EPOCH_START_HOUR_CHROME=1601
 EPOCH_START_MINUTE_CHROME=1
 EPOCH_START_SECOND_CHROME=1
 NOT_MEMORIZED_SYMBOL='X'
+MEMORIZED_SYMBOL='O'
+MEMORIZED_SYMBOL_TMP=''
--- rewordbook/manager/main.py
+++ rewordbook/manager/main.py
@@ -4,6 +4,7 @@ import csv
 import re
 import datetime
 import os
+from shutil import copyfile
 
 # featured history data
 history=[]
@@ -16,58 +17,73 @@ historyOrigin = fetcher.getSearchHitory(constants.CHROME)
 if os.path.isfile(constants.OUTPUT_URL):
 
     # bakcup origin file
-    os.rename(constants.OUTPUT_URL, constants.OUTPUT_BACKUP_URL);
+    copyfile(constants.OUTPUT_URL, constants.OUTPUT_BACKUP_URL)
+
+    # not memorized word
+    memorizedRecs = []
+    notMemorizedRecs = []
+    allWordInBook = []
+    with open(constants.OUTPUT_URL, 'r', newline='', encoding='UTF-8') as f:
+        reader = csv.reader(f)
+        for rec in reader:
+            allWordInBook.append(rec[1])
+            if rec[0] == constants.NOT_MEMORIZED_SYMBOL:
+                notMemorizedRecs.append(rec)
+            elif rec[0] == constants.MEMORIZED_SYMBOL_TMP:
+                rec[0] = constants.MEMORIZED_SYMBOL
+                memorizedRecs.append(rec)
 
     # add to output file
-    with open(constants.OUTPUT_URL, 'a+', newline='', encoding='UTF-8') as f:
+    with open(constants.OUTPUT_URL, 'w', newline='', encoding='UTF-8') as f:
         writer = csv.writer(f)
-        reader = csv.reader(f)
+        writer.writerow(constants.FIELDS)
         
         for recToCheck in historyOrigin:
-
             # create searched word field
             searchedWord = recToCheck[0].split("'")
             if len(searchedWord) > 1:
                 isNewWord = True
-                for recExist in reader:
-                    # if word is new and not memorized, need to add
-                    if searchedWord[1] is recExist[2]:
+                for existedRec in allWordInBook:
+                    # if word is new, need to add
+                    if searchedWord[1] == existedRec:
                         isNewWord = False
                         break
-                
                 if isNewWord is True:
-                    recToAdd = [None]*5
-                    recToAdd[0] = searchedWord[1]      
-                    recToAdd[1] = constants.NOT_MEMORIZED_SYMBOL
+                    newRecord = [None]*5
+                    newRecord[0] = constants.NOT_MEMORIZED_SYMBOL
+
+                    newRecord[1] = searchedWord[1]                    
 
                     # url field
-                    recToAdd[2] = recToCheck[1]
+                    newRecord[2] = recToCheck[1]
 
                     # date field : convert epoch timestamp to readable date
                     epoch_start = datetime.datetime(constants.EPOCH_START_HOUR_CHROME, constants.EPOCH_START_MINUTE_CHROME, constants.EPOCH_START_SECOND_CHROME)
                     delta = epoch_start + datetime.timedelta(microseconds=int(recToCheck[2]))
-                    recToAdd[3] = delta
+                    newRecord[3] = delta
 
                     # searched count field
-                    recToAdd[4] = recToCheck[3]
-                    writer.writerow(recToAdd)
-            # if searched word does't exist, skip
+                    newRecord[4] = recToCheck[3]
+                    writer.writerow(newRecord)
+            # if searched word doesn't exist, skip
             else:
                 continue
+
+        writer.writerows(notMemorizedRecs)
+        writer.writerows(memorizedRecs)
 
 # if already output file doens't exist
 else:
     for rec in historyOrigin:
         recTmp = [None]*5
+        recTmp[0] = constants.NOT_MEMORIZED_SYMBOL
 
         # create searched word field
         searchedWord = rec[0].split("'")
         if len(searchedWord) > 1:
-            recTmp[0] = searchedWord[1]
+            recTmp[1] = searchedWord[1]
         else:
             continue
-        
-        recTmp[1] = 'X'
 
         # url field
         recTmp[2] = rec[1]
